54f54d5c33b1ea2a166f8afdd0b9c7356fe555d8
Edward Arana <Edwardaranadlt@gmail.com>
2021-04-07 18:57:00 -0700

    adding info to components folder

--- employeedir/src/components/employeeDir.jsx
+++ employeedir/src/components/employeeDir.jsx
@@ -10,10 +10,33 @@ function employeeDir(props) {
                     <img src={props.employee.img} alt="Employee Pic" />
                 </td>
                 <td className="text-center align-middle">{props.employee.firstName + " " + props.employee.lastName}</td>
-                <td className="text-center align-middle"></td>
-                <td className="text-center align-middle"></td>
-                <td className="text-center align-middle"></td>
+                <td className="text-center align-middle">{props.employee.phone}</td>
+                <td className="text-center align-middle">{props.employee.cell}</td>
+                <td className="text-center align-middle">{props.employee.email}</td>
             </tr>
-        )
-    }
-}
+        );
+    };
+};
+
+return (
+    <div className="wrapper">
+        <table className="table table-striped table-hover">
+            <thead className="thread-dark">
+                <tr>
+                    <th data-name="img" onClick={props.sortTable}></th>
+                    <th data-name="firstName" onClick={props.sortTable}>Name </th>
+                    <th data-name="phone" onClick={props.sortTable}>Phone</th>
+                    <th data-name="cell" onClick={props.sortTable}>Cell</th>
+                    <th data-name="email" onClick={props.sortTable}>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.employee.map((x, i) =>(
+                <Row employee={x} key={i + "empRow"} />
+                ))}
+            </tbody>
+        </table>
+    </div>
+);
+
+export default employeeDir;
