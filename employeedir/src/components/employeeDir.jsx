b1c7ec2b97fcba627910adf0c868f242f0dda6e0
Edward Arana <Edwardaranadlt@gmail.com>
2021-04-07 18:41:34 -0700

    working on the components folder

--- employeedir/src/components/employeeDir.jsx
+++ employeedir/src/components/employeeDir.jsx
@@ -1,3 +1,19 @@
 import React from "react";
 import "./employeeDir.css"
 
+function employeeDir(props) {
+
+    const Row = (props) => {
+        return (
+            <tr>
+                <td className="align-middle">
+                    <img src={props.employee.img} alt="Employee Pic" />
+                </td>
+                <td className="text-center align-middle">{props.employee.firstName + " " + props.employee.lastName}</td>
+                <td className="text-center align-middle"></td>
+                <td className="text-center align-middle"></td>
+                <td className="text-center align-middle"></td>
+            </tr>
+        )
+    }
+}
